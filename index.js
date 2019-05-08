030d0e2ef75be4f4229c4fc92f216234e21608b9
Adriana Guerra <adriana.luz.guerra@gmail.com>
2019-05-08 19:17:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
+function sayHiToGrandma(string){
+  if (string.toUpperCase() === string)
+  {
+    return "YES INDEED!"
+  }
+  else if (string.toLowerCase() === string)
+  {
+    return "I can't hear you!"
+  }
+  if (string.toLowerCase() === "I love you, Grandma.")
+  {
+    return "I love you, too."
+  }
+}
